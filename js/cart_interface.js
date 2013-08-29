96158b0819625e435c58d230b09a58f1eb7a3d16
Mark Tertishniy <mtertishniy@gmail.com>
2013-08-30 00:03:49 +1100

    Add validation to cart amount input

--- js/cart_interface.js
+++ js/cart_interface.js
@@ -1,4 +1,9 @@
 $(function() {
+   $('td.quantity input').bind("change keyup input click", function(){
+      if (this.value.match(/^0|[^0-9]/g)){
+         this.value = this.value.replace(/^0|[^0-9]/g, '');
+      }
+   });
    $('#add_to_cart').click(function(){
       $id = $(this).attr('data');
       $.post(
@@ -40,10 +45,11 @@ $(function() {
    });
    $('td.quantity button').click(function(){
       $count = $(this).siblings('input').val();
+      $count = $count == 0 ? 1 : $count;
       $c_id = $(this).attr('data');
       $.post(
          '/includes/handlers/handler.Cart.php',
-         {  
+         { 
             mode: 'Update_count',
             g_count: $count,
             c_id: $c_id
